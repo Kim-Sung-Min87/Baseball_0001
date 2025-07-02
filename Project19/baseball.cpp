17b0697eb647b0206d75c957b6a8c42e5925aa0b
sungmini.kim <clever1501@naver.com>
2025-07-02 13:11:38 +0900

    [feature] page 1

--- Project19/baseball.cpp
+++ Project19/baseball.cpp
@@ -1,5 +1,10 @@
+#include <stdexcept>
 
+using namespace std;
 
 class Baseball {
-
+public:
+	void guess(const std::string& input) {
+		throw length_error("Must be three letters.");
+	}
 };
--- Project19/baseball_test.cpp
+++ Project19/baseball_test.cpp
@@ -3,6 +3,7 @@
 
 using namespace testing;
 
-TEST(Baseball, Test) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
